dadcd8b23b416eae423febfc9c77b43b6631f424
gustavo <gusfer@gmail.com>
2021-07-23 20:20:39 +0200

    Starting the synopse - draft

--- viewer/synopse.js
+++ viewer/synopse.js
@@ -1,7 +1,18 @@
 import pages from './pages.json' ;
 import fragments from './html_fragments.json';
 
-$.each(fragments, function(key){
+$.each(fragments['texts'], function(key){
     console.log(key);
     // ('#edition').append(fragments[val]);
+});
+
+$(document).ready(function(){
+    const current_url = window.location.search;
+    const urlParams = new URLSearchParams(current_url);
+    const spaltenzahl = urlParams.get('sp');
+    $('#spalten-sel-'+spaltenzahl).addClass('active');
+    for(var i=1; i <= spaltenzahl; i++){
+        $('#texts').append('<div class="col-md"><p>Hola</p></div>')
+    };
+    
 });
