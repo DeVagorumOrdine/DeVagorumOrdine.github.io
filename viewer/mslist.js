c1300c4b451576a8a408009840836f571254265c
gustavo <gusfer@gmail.com>
2021-08-31 11:36:17 +0200

    More responsiveness

--- viewer/mslist.js
+++ viewer/mslist.js
@@ -11,12 +11,12 @@ $(document).ready(function(){
             pages += '<a href="'+ 'edition.html?ms='+ key +'&pg='+ pg +'">' + pg + '</a>'
         })
         
-        $('#ms_list').append('<div class="list-group-item">'+
+        $('#ms_list').append('<div class="list-group-item col-md-6 col-sm-12">'+
         '<img class="mss-thumb m-3" src="https://devagorumordine.github.io/img/mss/'+
         key +'_'+ value['pages'][0] +'-thumb.jpg' +
         '"></img>'+
         '<h3 class="m-3">'+key + '</h3>'+
-        '<h4>'+ value['shelfmark']+'</h4>'+
+        '<h5>'+ value['shelfmark']+'</h5>'+
         pages + 
         '</div>')
     })
